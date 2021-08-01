e6a4283540a2a9bf2e07c6f446778f232bbfa60f
guddi <soumyashree.guddi@gmail.com>
2021-08-01 15:13:05 +0530

    hero

--- hero/settings.py
+++ hero/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
