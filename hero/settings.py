1733e3ee394871d16cc0e31acd4007ef159588dc
gudd1 <83784644+gudd1@users.noreply.github.com>
2023-06-04 17:41:20 +0530

    Update settings.py

--- hero/settings.py
+++ hero/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = '/static/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS=(
+'''STATICFILES_DIRS=(
    os.path.join(BASE_DIR, 'staticfiles'),
-)
+)'''
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
