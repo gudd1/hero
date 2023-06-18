1f7670f6d406cb038b9e95b8ac35a6076a28323d
gudd1 <83784644+gudd1@users.noreply.github.com>
2023-06-18 11:18:39 +0530

    Update settings.py

--- hero/settings.py
+++ hero/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-'''BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')'''
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 '''STATICFILES_DIRS=(
    os.path.join(BASE_DIR, 'staticfiles'),
