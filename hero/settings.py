2e90ec435fb6b85779ef29a1ff38aa967c014cc9
gudd1 <83784644+gudd1@users.noreply.github.com>
2023-06-18 10:50:55 +0530

    Update settings.py

--- hero/settings.py
+++ hero/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'hero.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
 
 # Password validation
@@ -127,10 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+'''BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')'''
+
 '''STATICFILES_DIRS=(
    os.path.join(BASE_DIR, 'staticfiles'),
 )'''
